9ff455ca72549c60616f6ea260937b082236a8a5
hasanabill <hasanabilna@gmail.com>
2023-07-26 23:58:51 +0600

    env removed

--- models/user.js
+++ models/user.js
@@ -1,1 +1,14 @@
 import { Schema, model, models } from 'mongoose'
+
+const UserSchema = newschema({
+    email: {
+        type: String,
+        unique: [true, 'Email already exists'],
+        required: [true, 'Email is required'],
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        match: []
+    }
+})
