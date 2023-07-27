3d630cbed01bb9094be167d1e42b54c24532a764
hasanabill <hasanabilna@gmail.com>
2023-07-27 09:29:47 +0600

    Stopped tracking .env File

--- utils/database.js
+++ utils/database.js
@@ -3,21 +3,21 @@ import mongoose from "mongoose";
 let isConnected = false;
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true);
+  mongoose.set("strictQuery", true);
 
-    if(isConnected){
-        console.log("MongoDB is connected");
-        return;
-    }
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dmName: "share_prompt",
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        isConnected = true;
-        console.log("MongoDB is connected");
-    } catch (error) {
-        console.log(error)
-    }
-}
+  if (isConnected) {
+    console.log("MongoDB is connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dmName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("MongoDB is connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
